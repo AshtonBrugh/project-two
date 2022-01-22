12dd9fd9278d75d1d25b8e6c706a7a7a835aa368
DSiedlarz90 <dsiedlarz90@gmail.com>
2022-01-22 09:52:01 -0500

    added all get/post/delete routes to  results and previous files

--- controllers/api/previous-routes.js
+++ controllers/api/previous-routes.js
@@ -1,12 +1,76 @@
 const router = require('express').Router();
 const { Previous } = require ('../../models');
 
-//get all results
-router.get('/', (req, res) => {
+//get all previous results
+router.get('/previous', (req, res) => {
     Previous.findAll({})
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
         console.log(err);
         res.status(500).json(err)
+    });
+});
+
+//find one previous result
+router.get('/previous/:id', (req, res) => {
+    Previous.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({ message: 'No result found with this id!' });
+            return;
+        }
+        res.json(dbUserData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// Add new previous result option
+router.post('/previous', (req, res) => {
+    Previous.create({
+        name: req.body.name,
+        quote: req.body.quote,
+        origin: req.body.origin,
+        personalities: req.body.personalities
+    })
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//delete a previousresult
+router.delete('/previous/:id', (req, res) => {
+    Previous.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No result found with this id! '});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
     });
 });
